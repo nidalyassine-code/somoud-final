5502f03ce585b8ee626cef69a81f194461eda27f
nidalyassine-code <nidal.yassine@gmail.com>
2026-01-28 17:42:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// هذا هو التنسيق الصحيح الذي سيحول العلامة الحمراء إلى خضراء
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
 })
